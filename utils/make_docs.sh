3b049f65dcc19f0acf4c6fff9c067dd0e9bd4809
wolffy.au <wolff_borg@yahoo.com.au>
2013-03-03 17:15:49 +1100

    [utils] - NaturalDocs templates

---
+++ utils/make_docs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PATH=$PATH:"/c/Program Files (x86)/NaturalDocs-1.52"
+NATURALDOCS=`which NaturalDocs 2>/dev/null`
+
+if [ -z "$NATURALDOCS" ]
+then
+  echo "Could not find NaturalDocs in the PATH. Install it or adjust the PATH environment variable."
+  exit 1
+fi
+
+echo -e "\n=== Compiling documentation ===\n"
+if [ ! -d ../store/function_library ]
+then
+  mkdir ../store/function_library
+fi
+
+"$NATURALDOCS" -i "../addons" -o HTML "../store/function_library" -p "ndocs-project" -s Default alive -r
+# --exclude-input "..\mso_ng.utes\CA
+
+echo -e "\n=== Packaging documentation ===\n"
+cd ../store
+rm -f function_library.tar
+tar -cf function_library.tar function_library
+
+exit 0
