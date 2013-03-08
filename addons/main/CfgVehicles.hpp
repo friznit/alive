150aadcaa549d2f0fd31d754b78d9f1679d59375
wolffy.au <wolff_borg@yahoo.com.au>
2013-03-08 20:45:37 +1100

    [main] - enabled ALIVE module categories

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -1,5 +1,32 @@
 // Add a game logic which does nothing except requires the addon in the mission.
 
+class CfgFactionClasses
+{
+        class AliveSystem
+        {
+                displayName = "$STR_ALIVE_SYS";
+                priority = 101;
+                side = 7;
+        };
+        class AliveSupport
+        {
+                displayName = "$STR_ALIVE_SUPPORT";
+                priority = 102;
+                side = 7;
+        };
+        class AliveAmbient
+        {
+                displayName = "$STR_ALIVE_AMBIENT";
+                priority = 103;
+                side = 7;
+        };
+        class AliveEnemy
+        {
+                displayName = "$STR_ALIVE_ENEMY";
+                priority = 104;
+                side = 7;
+        };
+};
 class CfgVehicles {
 	class Logic;
 	class Module_F: Logic
@@ -9,13 +36,13 @@ class CfgVehicles {
 			class Units;
 		};
 	};
-        class ModuleALIVEBase_F: Module_F
+        class ModuleAliveSystemBase: Module_F
         {
                 scope = 1;
-                displayName = "EditorALiVEBase";
-                category = "STR_ALIVE";
+                displayName = "EditorAliveSystemBase";
+                category = "AliveSystem";
         };
-	class ALiVE_require : ModuleALIVEBase_F {
+        class ALiVE_require : ModuleAliveSystemBase {
                 scope = 2;
 		displayName = "$STR_ALIVE_REQUIRES_ALIVE";
                 isGlobal = 1;
