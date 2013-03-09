8f8bb171d6cdbee4b9b48dee4395953a4e66f0da
wolffy.au <wolff_borg@yahoo.com.au>
2013-03-10 08:37:12 +1100

    [main] - Added map P: drive command file (this is for applications that need \x as the root direction, like Eliteness) - Using localised Website text on mod.cpp

---
+++ map_p_drive.cmd
@@ -0,0 +1,5 @@
+@echo off
+subst P: /D
+subst P: "C:\Program Files (x86)\Steam\SteamApps\common\Arma 3"
+
+pause
--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
 dir = "@ALIVE";
 name = "ArmA 3: ALiVE";
 picture = "x\alive\addons\main\logo_alive.paa";
-actionName = "Website";
+actionName = "$STR_MOD_LAUNCHER_ACTION";
 action = "http://dev-heaven.net/projects/alive";
 description = "Bugtracker: https://dev-heaven.net/projects/alive/issues<br/>Documentation: https://dev-heaven.net/projects/alive/documents";
