7a6ff60b82adc7c0d86b63860969c587e7ceecee
wolffy.au <wolff_borg@yahoo.com.au>
2013-03-17 20:45:34 +1100

    [main] - Added isServerAdmin function

--- addons/main/CfgFunctions.hpp
+++ addons/main/CfgFunctions.hpp
@@ -3,6 +3,7 @@ class cfgFunctions {
                 class COMPONENT {
                         file = "\x\alive\addons\main\functions";
 			class baseClass {description = "Base class";};
+			class isServerAdmin {description = "Checks if a player is currently logged in as server admin.";};
                 };
         };
 };
