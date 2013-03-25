104473a4727606c5057f715f42714b7f1f4e062d
wolffy.au <wolff_borg@yahoo.com.au>
2013-03-25 22:22:58 +1100

    [viewdistance] - Added max/min viewdistance and terraingrid attributes in editor module -

--- addons/sys_viewdistance/CfgPatches.hpp
+++ addons/sys_viewdistance/CfgPatches.hpp
@@ -8,7 +8,7 @@ class CfgPatches {
 		versionDesc = "ALiVE";
 		versionAct = "['SYS_VIEWDISTANCE',_this] execVM '\x\alive\addons\main\about.sqf';";
 		VERSION_CONFIG;
-		author[] = {"Gunny"};
+		author[] = {"Gunny", "Wolffy.au"};
 		authorUrl = "http://dev-heaven.net/projects/alive";
 	};
 };
--- addons/sys_viewdistance/CfgVehicles.hpp
+++ addons/sys_viewdistance/CfgVehicles.hpp
@@ -11,31 +11,30 @@ class CfgVehicles {
                 picture = "x\alive\addons\sys_viewdistance\icon_sys_viewdistance.paa";
                 class Arguments
                 {
-                        class vdist
+                        class maxVD
                         {
-                                displayName = "$STR_ALIVE_VDIST_ENABLED";
-                                description = "$STR_ALIVE_VDIST_ENABLED_COMMENT";
-                                class Values
-                                {
-                                        class Yes
-                                        {
-                                                name = "Yes";
-                                                value = 1;
-                                                default = 1;
-                                        };
-                                        class No
-                                        {
-                                                name = "No";
-                                                value = 0;
-                                        };
-                                };
+                                displayName = "$STR_ALIVE_VDIST_MAX";
+                                description = "$STR_ALIVE_VDIST_MAX_COMMENT";
+				defaultvalue = "20000";
                         };
-                          class Condition
+                        class minVD
                         {
-                                displayName = "Condition:";
-                                description = "";
-                                defaultValue = "true";
-                        };                        
+                                displayName = "$STR_ALIVE_VDIST_MIN";
+                                description = "$STR_ALIVE_VDIST_MIN_COMMENT";
+				defaultvalue = "500";
+                        };
+                        class maxTG
+                        {
+                                displayName = "$STR_ALIVE_TGRID_MAX";
+                                description = "$STR_ALIVE_TGRID_MAX_COMMENT";
+				defaultvalue = "50";
+                        };
+                        class minTG
+                        {
+                                displayName = "$STR_ALIVE_TGRID_MIN";
+                                description = "$STR_ALIVE_TGRID_MIN_COMMENT";
+				defaultvalue = "5";
+                        };
                 };
                 
         };
