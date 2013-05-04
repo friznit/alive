ba7cf495cad9b8ed1176a959adb82913390ea6dd
wolffy.au <wolff_borg@yahoo.com.au>
2013-05-04 23:04:06 +1000

    [utils] - Updated Map P: drive

--- utils/tools/ALIVE_map_p_drive.cmd
+++ utils/tools/ALIVE_map_p_drive.cmd
@@ -4,6 +4,6 @@ IF NOT EXIST P:\NUL GOTO MAP_P
 subst P: /D 
 
 :MAP_P
-subst P: "C:\Users\Wolffy.au\Documents\Arma 3 Alpha\Missions"
+subst P: "C:\Users\%USERNAME%\Documents\Arma 3 Alpha\Missions"
 
 rem pause
