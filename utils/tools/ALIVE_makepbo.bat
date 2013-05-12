25fdfedddc6e88761d4984b390912d30c19c984a
wolffy.au <wolff_borg@yahoo.com.au>
2013-05-12 22:32:53 +1000

    [utils] - makePBO moves PBO's to @Alive addons folder

--- utils/tools/ALIVE_makepbo.bat
+++ utils/tools/ALIVE_makepbo.bat
@@ -1,12 +1,16 @@
 @echo off
 rem set exe=echo
-set exe=MakePBO.exe -A -Z -BD -L -G
+set exe=MakePBO.exe -A -BD -L -G
+rem -Z
 set source=P:\x\alive\addons
+set target="C:\Program Files (x86)\Steam\SteamApps\common\Arma 3\@ALiVE\addons"
 
 FOR /F "tokens=1* delims=," %%A in ('dir %source% /ad /b') do (
 	%exe% "%source%\%%A"
 	if ERRORLEVEL 1 goto err
 )
+
+move /Y %source%\*.pbo %target%\
 
 goto end
 
