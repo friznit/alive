7be5c0757d393dba1649715f9d32ac920b225144
wolffy.au <wolff_borg@yahoo.com.au>
2013-05-15 22:25:12 +1000

    [utils] - Reverted because BIS reverted

--- utils/tools/ALIVE_map_p_drive.cmd
+++ utils/tools/ALIVE_map_p_drive.cmd
@@ -4,6 +4,6 @@ IF NOT EXIST P:\NUL GOTO MAP_P
 subst P: /D 
 
 :MAP_P
-subst P: "C:\Users\%USERNAME%\Documents\Arma 3\Missions"
+subst P: "C:\Users\%USERNAME%\Documents\Arma 3 Alpha\Missions"
 
 rem pause
