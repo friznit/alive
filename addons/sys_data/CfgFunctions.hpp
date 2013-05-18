fde05a5d0b49c197d9e812ef74869246162bb980
wolffy.au <wolff_borg@yahoo.com.au>
2013-05-19 07:31:18 +1000

    [sys_data] - Minor lint fixes

--- addons/sys_data/CfgFunctions.hpp
+++ addons/sys_data/CfgFunctions.hpp
@@ -1,26 +1,26 @@
 class cfgFunctions {
         class PREFIX {
                 class COMPONENT {
-						class convertData {
-							description = "Decomposes objects and data to a text string";
-							 file = "\x\alive\addons\sys_data\fnc_convertData.sqf";
-						};
-						class restoreData {
-							description = "Composes objects and data from a text string";
-							 file = "\x\alive\addons\sys_data\fnc_restoreData.sqf";
-						};
-						class sendToPlugIn {
-							description = "Sends data to an external plugin via arma2net";
-							 file = "\x\alive\addons\sys_data\fnc_sendToPlugIn.sqf";
-						};
-						class writeData {
-							description = "Writes data to a data source";
-							file = "\x\alive\addons\sys_data\fnc_writeData.sqf";
-						};
-						class readData {
-							description = "Reads data from a data source";
-							 file = "\x\alive\addons\sys_data\fnc_readData.sqf";
-						};
+			class convertData {
+				description = "Decomposes objects and data to a text string";
+				 file = "\x\alive\addons\sys_data\fnc_convertData.sqf";
+			};
+			class restoreData {
+				description = "Composes objects and data from a text string";
+				 file = "\x\alive\addons\sys_data\fnc_restoreData.sqf";
+			};
+			class sendToPlugIn {
+				description = "Sends data to an external plugin via arma2net";
+				 file = "\x\alive\addons\sys_data\fnc_sendToPlugIn.sqf";
+			};
+			class writeData {
+				description = "Writes data to a data source";
+				file = "\x\alive\addons\sys_data\fnc_writeData.sqf";
+			};
+			class readData {
+				description = "Reads data from a data source";
+				 file = "\x\alive\addons\sys_data\fnc_readData.sqf";
+			};
                 };
         };
 };
