a046a69cd8a005a3109557e4fdd8d7dd529afe76
Tupolov <tupolov73@gmail.com>
2013-08-07 14:12:23 +0100

    [sys_weaponrest] - Removed

--- addons/sys_weaponrest/eventhandlers.hpp
+++
@@ -1,7 +0,0 @@
-class Extended_PreInit_EventHandlers
-{
-	class alive_sys_weaponrest
-	{
-		clientInit = "call ('\x\alive\addons\sys_weaponrest\XEH_preClientInit.sqf' call SLX_XEH_COMPILE)";
-	};
-};
