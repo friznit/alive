277647fa8703757a5e1ec6aa58893f34f4e84f56
highhead <1enterprise2>
2013-08-11 18:33:32 +0200

    [MIL_OPCOM] Added OPCOM showcase

--- addons/mil_OPCOM/config.cpp
+++ addons/mil_OPCOM/config.cpp
@@ -3,3 +3,30 @@
 #include <CfgPatches.hpp>
 #include <CfgVehicles.hpp>
 #include <CfgFunctions.hpp>
+
+class CfgMissions
+{
+        class Challenges{};
+        class Missions
+        {
+                displayName = "$STR_A3_CFGMISSIONS_MISSIONS0";
+                overview = "";
+        };
+        class Showcases
+        {
+                displayName = "$STR_A3_CFGMISSIONS_SHOWCASES0";
+                briefingName = "$STR_A3_CFGMISSIONS_SHOWCASES0";
+                author = "$STR_A3_Bohemia_Interactive";
+                overviewPicture = "a3\Missions_F_Beta\data\img\Campaign_overview_CA.paa";
+                overviewText = "$STR_A3_CFGMISSIONS_SHOWCASES0";
+                
+					class showcase_mil_opcom
+					{
+						briefingName = "ALiVE OPCOM Spleen";
+						directory = "x\alive\addons\mil_opcom\showcases\opcom_spleen.stratis";
+						overviewPicture = "x\alive\addons\mil_opcom\showcases\opcom_spleen.stratis\rsc\logo_alive.paa";
+					};
+        };
+};
+
+
