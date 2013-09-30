67befcbeac0cbf5eb6182e32317d14b2c917717b
VRCRaptor <c.oltermann@gmail.com>
2013-09-29 22:32:01 -0500

    [@ALiVE] create a mod.cpp file for the site with description and website information - draft #1

---
+++ addons/mod.cpp
@@ -0,0 +1,5 @@
+﻿name = "ALiVE - Advanced Light Intanftry Virtual Environment";
+picture = "images\alive_logo_crop.png";
+actionName = "Website";
+action = "http://www.aram3live.com";
+description = "From the developers of MSO comes, [ALiVE] -- Advanced Light Infantry Virtual Environment -- which takes MSO to the next level by bringing performance and simulation within the ARMA 3 universe, ALiVE.";
