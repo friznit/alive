0be0629fb2fb10cc6bdd1255f669e272ecbaa009
VRCRaptor <c.oltermann@gmail.com>
2013-09-29 22:59:43 -0500

    [@ALiVE] images changes and added alive_logo.paa, these need to be included in the @ALiVE folder before the addons folder... when you compile them yourself  -  draft #2

--- addons/mod.cpp
+++ addons/mod.cpp
@@ -1,5 +1,5 @@
 ﻿name = "ALiVE - Advanced Light Intanftry Virtual Environment";
-picture = "images\alive_logo_crop.png";
+picture = "alive_logo.paa";
 actionName = "Website";
 action = "http://www.aram3live.com";
 description = "From the developers of MSO comes, [ALiVE] -- Advanced Light Infantry Virtual Environment -- which takes MSO to the next level by bringing performance and simulation within the ARMA 3 universe, ALiVE.";
