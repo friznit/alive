aa1b88f73fa4d0fe3048157de51be43ef691eccc
highhead <1enterprise2>
2013-10-14 22:37:25 +0200

    [SYS_GC] Implemented proper GarbageCollector

---
+++ addons/sys_GC/CfgVehicles.hpp
@@ -0,0 +1,43 @@
+class CfgVehicles {
+        class ModuleAliveSystemBase;
+        class ADDON : ModuleAliveSystemBase
+        {
+                scope = 2;
+                displayName = "$STR_ALIVE_GC";
+                function = "ALIVE_fnc_GCInit";
+				functionPriority = 4;
+                isGlobal = 1;
+                isPersistent = 1;
+				icon = "x\alive\addons\sys_GC\icon_sys_GC.paa";
+				picture = "x\alive\addons\sys_GC\icon_sys_GC.paa";
+                class Arguments
+                {
+                        class debug
+                        {
+                                displayName = "$STR_ALIVE_GC_DEBUG";
+                                description = "$STR_ALIVE_GC_DEBUG_COMMENT";
+                                class Values
+                                {
+                                        class Yes
+                                        {
+                                                name = "Yes";
+                                                value = true;
+                                                default = true;
+                                        };
+                                        class No
+                                        {
+                                                name = "No";
+                                                value = false;
+                                        };
+                                };
+                        };
+                        class ALiVE_GC_INDIVIDUALTYPES
+                        {
+                                displayName = "$STR_ALIVE_GC_INDIVIDUALTYPES";
+                                description = "$STR_ALIVE_GC_INDIVIDUALTYPES_COMMENT";
+                                defaultValue = [];
+                        }
+                 };
+                
+        };
+};
