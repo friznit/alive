8be4782b65082ee9715b9489ff453b75cc9968c2
ARjay <arjaydev@gmail.com>
2013-10-27 15:44:39 +1100

    [sys_crewinfo] bug fix

--- addons/sys_crewinfo/CfgFunctions.hpp
+++ addons/sys_crewinfo/CfgFunctions.hpp
@@ -3,12 +3,12 @@
                 class COMPONENT {
                         class crewinfo {
                                 description = "The main class";
-                                file = "sys_crewinfo\fnc_crewinfo.sqf";
+                                file = "\x\alive\addons\sys_crewinfo\fnc_crewinfo.sqf";
 																recompile = 1;
                         };
                         class crewinfoInit {
                                 description = "The module initialisation function";
-                                file = "sys_crewinfo\fnc_crewinfoInit.sqf";
+                                file = "\x\alive\addons\sys_crewinfo\fnc_crewinfoInit.sqf";
 																recompile = 1;
                         };  
                 };
