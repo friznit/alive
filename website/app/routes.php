e1dbb707edb5aaa96ac38e70165bd0f2b5936f4f
ARjay <arjaydev@gmail.com>
2013-11-16 00:16:59 +1100

    [website] initial commit

---
+++ website/app/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::controller('/auth', 'AuthController');
+
+Route::get('style', function()
+{
+    return View::make('style/index');
+});
+
+Route::get('/', function()
+{
+    return View::make('home/index');
+});
+
+//Route::controller('/', 'HomeController');
---
+++ website/public/js/alive_home.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+});
+
+$(function(){
+
+    // smooth scroll
+    $("#topnav ul li a[href^='#']").on('click', function(e) {
+
+        // prevent default anchor click behavior
+        e.preventDefault();
+
+        // store hash
+        var hash = this.hash;
+
+        // animate
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 300, function(){
+
+            // when done, add hash to url
+            // (default click behaviour)
+            window.location.hash = hash;
+        });
+
+    });
+
+    $("#login").on('click', function(e) {
+        alert('FOO');
+    });
+
+});
+
