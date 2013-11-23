a812ab1e1865655f972853fc3421b4aaadc834c3
highhead <1enterprise2>
2013-11-23 17:47:21 +0100

    [SUP_COMBATSUPPORT] Added Module description to be shown on show infor button, added USAGE in stringtable, added module author

--- addons/sup_combatsupport/CfgVehicles.hpp
+++ addons/sup_combatsupport/CfgVehicles.hpp
@@ -5,11 +5,12 @@ class CfgVehicles {
                 scope = 2;
                 displayName = "$STR_ALIVE_COMBATSUPPORT";
                 function = "ALIVE_fnc_CombatSupportInit";
+                author = MODULE_AUTHOR;
                 isGlobal = 1;
                 isPersistent = 1;
                 icon = "x\alive\addons\sup_combatsupport\icon_sup_combatsupport.paa";
                 picture = "x\alive\addons\sup_combatsupport\icon_sup_combatsupport.paa";
-                 class Arguments
+                class Arguments
                 {
                         class combatsupport_item
                         {
@@ -18,7 +19,40 @@ class CfgVehicles {
                                 defaultValue = "LaserDesignator";
                         };
                 };
-                
+                class ModuleDescription
+				{
+					//description = "$STR_ALIVE_CS_COMMENT"; // Short description, will be formatted as structured text
+					description[] = {
+							"$STR_ALIVE_CS_COMMENT",
+							"",
+							"$STR_ALIVE_CS_USAGE"
+					};
+					sync[] = {"ALiVE_SUP_TRANSPORT","ALiVE_SUP_CAS"}; // Array of synced entities (can contain base classes)
+		 
+					class ALiVE_SUP_TRANSPORT
+					{
+						description[] = { // Multi-line descriptions are supported
+							"$STR_ALIVE_TRANSPORT_COMMENT",
+							"",
+							"$STR_ALIVE_TRANSPORT_USAGE"
+						};
+						position = 1; // Position is taken into effect
+						direction = 1; // Direction is taken into effect
+						optional = 1; // Synced entity is optional
+						duplicate = 1; // Multiple entities of this type can be synced
+					};
+					class ALiVE_SUP_CAS
+					{
+						description[] = { // Multi-line descriptions are supported
+							"$STR_ALIVE_CAS_COMMENT",
+							"",
+							"$STR_ALIVE_CAS_USAGE"
+						};
+						position = 1; // Position is taken into effect
+						direction = 1; // Direction is taken into effect
+						optional = 1; // Synced entity is optional
+						duplicate = 1; // Multiple entities of this type can be synced
+					};
+				};
         };
-
 };
