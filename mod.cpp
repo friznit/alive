86d69cc8705572fde40e0a45127c08a5c55659f5
wolffy.au <wolff_borg@yahoo.com.au>
2013-11-24 21:57:18 +1100

    [main] - Updated website

--- mod.cpp
+++ mod.cpp
@@ -2,5 +2,5 @@ dir = "@ALIVE";
 name = "ArmA 3: ALiVE";
 picture = "x\alive\addons\main\logo_alive.paa";
 actionName = "$STR_MOD_LAUNCHER_ACTION";
-action = "http://dev-heaven.net/projects/alive";
+action = "http://alivemod.com/";
 description = "Bugtracker: https://dev-heaven.net/projects/alive/issues<br/>Documentation: https://dev-heaven.net/projects/alive/documents";
