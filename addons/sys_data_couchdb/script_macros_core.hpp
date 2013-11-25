240c3bcce1eb9d7a3a3f92651aef3a84ad6c6568
wolffy.au <wolff_borg@yahoo.com.au>
2013-11-25 21:26:44 +1100

    [all] - Removed sys_data* & sys_statistics

--- addons/sys_data_couchdb/script_macros_core.hpp
+++
@@ -1,6 +0,0 @@
-#define PREFIX ALIVE
-#define COMPONENT sys_data_couchdb
-
-#define EXECNOW call compile preprocessfilelinenumbers
-
-#include <\x\cba\addons\main\script_macros_mission.hpp>
--- addons/sys_data_sql/script_macros_core.hpp
+++
@@ -1,6 +0,0 @@
-#define PREFIX ALIVE
-#define COMPONENT sys_data_sql
-
-#define EXECNOW call compile preprocessfilelinenumbers
-
-#include <\x\cba\addons\main\script_macros_mission.hpp>
