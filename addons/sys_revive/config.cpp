a6c98e304c33e166422961fa9d583d5dd7f374e5
wolffy.au <wolff_borg@yahoo.com.au>
2013-11-26 23:20:22 +1100

    [sys_revive] - Removed from release

--- addons/sys_revive/config.cpp
+++
@@ -1,9 +0,0 @@
-#include <script_component.hpp>
-#include <\x\alive\addons\main\CfgMods.hpp>
-#include <CfgPatches.hpp>
-#include <CfgVehicles.hpp>
-#include <CfgFunctions.hpp>
-
-/* revive dialog conifg */
-#include <revive_dialog.h>
-
--- addons/sys_revive/sys_revive/release_body.sqf
+++
@@ -1,2 +0,0 @@
-// Variable monitoring drag_body.sqf giving the signal for releasing the body
-REV_Release_Body = true;
