c2daf84f4fc86adc8063985adf2e4059367de9a8
Gunny <kev@kevingunn.co.uk>
2013-12-13 18:12:03 +0000

    [SUP_ARTILLERY] Added correct FSM and put back correct map_p

--- utils/tools/ALIVE_map_p_drive.cmd
+++ utils/tools/ALIVE_map_p_drive.cmd
@@ -4,4 +4,6 @@ IF NOT EXIST P:\NUL GOTO MAP_P
 subst P: /D 
 
 :MAP_P
-subst P: "C:\Users\%USERNAME%\Documents\Arma 3\missions"rem pause
+subst P: "C:\Users\%USERNAME%\Documents\Arma 3\missions"
+
+rem pause
