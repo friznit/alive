09aa0d7d3a48723bcf0dc3f07adf5df97248b971
Tupolov <tupolov73@gmail.com>
2013-12-17 12:15:21 +0000

    [website] Updated overview table view

--- website/app/views/warroom/tables/overview.blade.php
+++ website/app/views/warroom/tables/overview.blade.php
@@ -4,16 +4,16 @@
 
         $.getJSON('http://msostore.iriscouch.com/events/_design/events/_view/Totals?callback=?', function(totals)
         {
-            $('#overview').append('EKIA: ' + totals.rows[0].value.Kills + '<br>')
-            $('#overview').append('Losses: ' + totals.rows[0].value.Deaths+ '<br>')
-            $('#overview').append('Operations: ' + totals.rows[0].value.Operations + '<br>')
-            $('#overview').append('Combat Hours: ' + Math.round((totals.rows[0].value.CombatHours / 60)*10)/10 + '<br>')
-            $('#overview').append('Ammunition: ' + totals.rows[0].value.ShotsFired + '<br>')
+            $('#overview').append('EKIA: ' + totals.rows[0].value.Kills + ' | ')
+            $('#overview').append('Losses: ' + totals.rows[0].value.Deaths+ ' | ')
+            $('#overview').append('Operations: ' + totals.rows[0].value.Operations + ' | ')
+            $('#overview').append('Combat Hours: ' + Math.round((totals.rows[0].value.CombatHours / 60)*10)/10 + ' | ')
+            $('#overview').append('Ammunition: ' + totals.rows[0].value.ShotsFired + ' | ')
         })
 
         $.getJSON('http://msostore.iriscouch.com/events/_design/events/_view/players_list?group_level=2&callback=?', function(activeunits)
         {
-            $('#overview').append('Active Units: ' + activeunits.rows.length + '<br>')
+            $('#overview').append('Active Units: ' + activeunits.rows.length)
         })
     })
 </script>
