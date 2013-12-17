1a1025fbcb5459dd589495ca5e141bd8437e017f
Tupolov <tupolov73@gmail.com>
2013-12-17 15:41:40 +0000

    [website] - Updated nav bar for War Room and admin views, using logos for access to alive site or war room.

--- website/app/views/admin/partials/_nav.blade.php
+++ website/app/views/admin/partials/_nav.blade.php
@@ -7,19 +7,17 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ URL::to('/war-room') }}"><img src="{{ URL::to('/') }}/img/warroom_logo_combined.png"/></a>
+            <a class="navbar-brand" href="{{ URL::to('/') }}"><img src="{{ URL::to('/') }}/img/logo.png"/></a>
+            <a class="navbar-brand" href="{{ URL::to('/war-room') }}"><img src="{{ URL::to('/') }}/img/alive_warroom_tiny.png"/></a>
         </div>
         <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="{{{ URL::to('/') }}}/">ALiVE</a></li>
-                <li{{ (Request::is('war-room') ? ' class="active"' : '') }}><a href="{{{ URL::to('war-room') }}}/">War Room</a></li>
-            </ul>
             <ul class="nav navbar-nav pull-right">
 
                 <li {{ (Request::is('admin/user/show/*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/user/show/') }}/{{ Sentry::getUser()->getId() }}">Profile</a></li>
 
                 @if ($auth['isAdmin'])
                 <li {{ (Request::is('admin/clan*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/clan') }}">Groups</a></li>
+                <li {{ (Request::is('admin/server*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/server') }}">Servers</a></li>
                 <li {{ (Request::is('admin/user*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/user') }}">Users</a></li>
                 <li {{ (Request::is('admin/group*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/group') }}">User Groups</a></li>
                 @endif
