a88f0fe49e4b536c0ac96440c4fd40a2fb887b4c
Tupolov <tupolov73@gmail.com>
2013-12-20 22:16:35 +0000

    [website] Added leaflets.js test map and code

--- website/app/views/warroom/home/index.blade.php
+++ website/app/views/warroom/home/index.blade.php
@@ -2,48 +2,35 @@
 
 {{-- Content --}}
 @section('content')
-
-<div id="map" style="max-width: 100%"></div>
+ 
+<div id="map" style="height: 900px;"></div>
 
 <script type="text/javascript">
-	var map;
-	var mapBounds = new OpenLayers.Bounds( 0.0, -1410.0, 3300, 0.0); // (  0.0, -2682.0, 4496.0, 0.0 )
-	var mapMinZoom = 0;
-	var mapMaxZoom = 5; // 6
 
-	// avoid pink tiles
-	OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
-	OpenLayers.Util.onImageLoadErrorColor = "transparent";
-
-	function init(){
-		var options = {
-			controls: [],
-			maxExtent: new OpenLayers.Bounds(  0.0, -1410.0, 3300, 0.0 ), //  (  0.0, -2682.0, 4496.0, 0.0 )
-			maxResolution: 16.000000, //32
-			numZoomLevels: 6 // 7
-			};
-		map = new OpenLayers.Map('map', options);
-	
-		var layer = new OpenLayers.Layer.TMS( "ALiVE Global","{{ URL::to('/') }}/maps/globalmap2/", //globalmap
-			{  url: '', serviceVersion: '.', layername: '.', alpha: true,
-				type: 'png', getURL: overlay_getTileURL
-			});
-		map.addLayer(layer);
-		map.zoomToExtent( mapBounds );	
-	
-		//map.addControl(new OpenLayers.Control.PanZoomBar());
-		//map.addControl(new OpenLayers.Control.MousePosition());
-		map.addControl(new OpenLayers.Control.MouseDefaults());
-		map.addControl(new OpenLayers.Control.KeyboardDefaults());
-	}
-	
-	onresize=function(){ resize(); };
-			
-	$(document).ready(function() {
-		init();
-	    resize();
-		map.setCenter(new OpenLayers.LonLat(1600,700), 4); // 
-
+        var map = L.map('map', {
+			minZoom: 2,
+			maxZoom: 5,
+			crs: L.CRS.Simple
+		}).setView([2048,2048], 2);
+		
+		var southWest = map.unproject([0,4096], map.getMaxZoom());
+		var northEast = map.unproject([4096,0], map.getMaxZoom());
+		map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+		L.tileLayer("{{ URL::to('/') }}/maps/globalmap3/{z}/{x}/{y}.png" , {
+            attribution: 'ALiVE',
+            tms: true	//means invert.
+        }).addTo(map);
+		
+		map.on('click', function(e) {
+			alert(map.project(e.latlng, map.getMaxZoom()));
+		});
+		
+    $(document).ready(function() {
+		$(".trigger").click(function(){
+			$(".panel").toggle("fast");
+			$(this).toggleClass("active");
+			return false;
+		});
 	});
 </script>
 
@@ -55,7 +42,7 @@
     @include('warroom/tables/recent_ops')
 </div>
 
-<div class="col-md-3" style="position: absolute; z-index:10000; left: 10px; top:300px;">
+<div class="col-md-2" style="position: absolute; z-index:10000; left: 10px; top:300px;">
     @include('warroom/tables/t1operators')
 </div>
 
@@ -63,7 +50,7 @@
     @include('warroom/tables/live_feed')
 </div>
 
-<div class="col-md-12" style="position: absolute; z-index:10000; bottom:100px;">
+<div class="col-md-12">
 
                 <div class="col-md-3">
                     @include('warroom/charts/blu_losses')
--- website/app/views/warroom/tables/t1operators.blade.php
+++ website/app/views/warroom/tables/t1operators.blade.php
@@ -1,31 +1,18 @@
 <script>
     $(document).ready(function(){
         $('#t1_operators').dataTable({
-            "bJQueryUI": true,
-            "sAjaxSource": 'http://msostore.iriscouch.com/events/_design/kill/_view/player_kills_count?group_level=1&callback=?',
-            "sAjaxDataProp": "rows",
-            "bPaginate": true,
-			"bFilter": true,
-            "bInfo": false,
-            "aaSorting": [[1, "desc" ]],
-            "fnDrawCallback": function ( oSettings ) {
-                $("#t1operators_container").mCustomScrollbar("update");
-            },
-                /* Need to redo the counters if filtered or sorted */
-                /*
-                if ( oSettings.bSorted || oSettings.bFiltered )
-                {
-                    for ( var i=0, iLen=oSettings.aiDisplay.length ; i<iLen ; i++ )
-                    {
-                        $('td:eq(0)', oSettings.aoData[ oSettings.aiDisplay[i] ].nTr ).html( i+1 );
-                    }
-                }
-                */
-            "aoColumnDefs": [
-                { "mDataProp": "key",  "aTargets": [ 0 ]},
-                { "mDataProp": "value", "aTargets": [ 1 ]}
-            ]
-        } );
+					"bJQueryUI": true,
+					"sAjaxSource": 'http://msostore.iriscouch.com/events/_design/kill/_view/player_kills_count?group_level=1&callback=?',
+					"sAjaxDataProp": "rows",
+					"sScrollY": "500px",
+					"bPaginate": false,
+					"bScrollCollapse": true,
+					"aaSorting": [[1, "desc" ]],
+					"aoColumnDefs": [
+						{ "mDataProp": "key",  "aTargets": [ 0 ]},
+						{ "mDataProp": "value", "aTargets": [ 1 ]}
+					]
+				} );
     });
 
 </script>
@@ -33,7 +20,7 @@
 <table cellpadding="0" cellspacing="0" border="0" class="dataTable table" id="t1_operators">
     <thead>
     <tr>
-        <th width="80%">Player</th>
+        <th width="70%">Player</th>
         <th>EKIA</th>
     </tr>
     </thead>
