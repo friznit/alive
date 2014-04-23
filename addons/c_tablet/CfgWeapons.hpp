7b9d55ac3e0c9d5602611aa54983061b08d28c69
Tupolov <tupolov73@gmail.com>
2014-04-23 20:47:11 +0100

    [tablet] Added tablet model (m_tablet) and config (c_tablet). Still WIP as RVMATs not showing in game yet.

---
+++ addons/c_tablet/CfgWeapons.hpp
@@ -0,0 +1,20 @@
+class CfgWeapons {
+	class ItemCore;
+	class InventoryItem_Base_F;
+	class TabletItem: InventoryItem_Base_F
+	{
+		type = 1001;
+	};
+	class ALIVE_Tablet: ItemCore
+	{
+		scope = 2;
+		displayName = "$STR_ALIVE_Tablet";
+		picture = "\x\alive\addons\m_tablet\data\UI\gear_tablet_CA.paa";
+		model = "\x\alive\addons\m_tablet\Tablet";
+		descriptionShort = "$STR_ALIVE_Tablet1";
+		class ItemInfo: TabletItem
+		{
+			mass = 8;
+		};
+	};
+};
