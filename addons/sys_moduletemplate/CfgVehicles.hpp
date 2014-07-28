6a026d0908ac9b3b26953c3104d753f38e3e7071
Cameroon <chris@cjbehm.com>
2014-07-28 13:02:34 -0400

    Create an auto-enabling template module
    This module provides a minimal template that can be used as a starting
    point for new modules.

---
+++ addons/sys_moduletemplate/CfgVehicles.hpp
@@ -0,0 +1,60 @@
+class CfgVehicles {
+        class ModuleAliveSystemBase;
+        
+        class ADDON : ModuleAliveSystemBase
+        {
+        		scope = 1;
+				displayName = "$STR_ALIVE_MODULETEMPLATE";
+				//function = "";
+				functionPriority = 0;
+				isGlobal = 2;
+				icon = "x\alive\addons\sys_moduletemplate\icon_sys_moduletemplate.paa";
+				picture = "x\alive\addons\sys_moduletemplate\icon_sys_moduletemplate.paa";
+				author = MODULE_AUTHOR;
+        };
+
+        class ALiVE_SYS_MODULETEMPLATEPARAMS : ModuleAliveSystemBase
+        {
+				scope = 2;
+				displayName = "$STR_ALIVE_MODULETEMPLATEPARAMS";
+				function = "ALIVE_fnc_moduleTemplateParams";
+				functionPriority = 0;
+				isGlobal = 2;
+				icon = "x\alive\addons\sys_moduletemplate\icon_sys_moduletemplate.paa";
+				picture = "x\alive\addons\sys_moduletemplate\icon_sys_moduletemplate.paa";
+				author = MODULE_AUTHOR;
+				
+                class ModuleDescription
+				{
+					description[] = {
+							"$STR_ALIVE_MODULETEMPLATEPARAMS_COMMENT",
+							"",
+							"$STR_ALIVE_MODULETEMPLATEPARAMS_USAGE"
+					};
+				};
+				
+                class Arguments
+                {
+
+                        class DEBUG
+                        {
+                                displayName = "$STR_ALIVE_MODULETEMPLATE_DEBUG";
+                                description = "$STR_ALIVE_MODULETEMPLATE_DEBUG_COMMENT";
+                                class Values
+                                {
+                                        class Yes
+                                        {
+                                                name = "Yes";
+                                                value = true;
+                                        };
+                                        class No
+                                        {
+                                                name = "No";
+                                                value = false;
+                                                default = 1;
+                                        };
+                                };
+                        };
+                };
+        };
+};
