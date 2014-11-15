731d4e040da8097bf21d715b6e96ae8282b7553e
highhead <1enterprise2>
2014-11-15 12:12:22 +0100

    [SYS MARKER] Changed to emptyInit function (fixed warnings)

--- addons/sys_marker/CfgVehicles.hpp
+++ addons/sys_marker/CfgVehicles.hpp
@@ -5,7 +5,7 @@ class CfgVehicles {
         {
         		scope = 1;
 				displayName = "$STR_ALIVE_MARKER";
-				//function = "";
+				function = "ALIVE_fnc_emptyInit";
 				functionPriority = 44;
 				isGlobal = 2;
 				icon = "x\alive\addons\sys_marker\icon_sys_marker.paa";
