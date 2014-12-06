a269c5cdd9fab338c0e69e055b59f73bcc1e6a77
highhead <1enterprise2>
2014-12-06 04:57:43 +0100

    [MIL IED | SYS NEWSFEED] Fixed recompile = 1 was set static instead of main recompile variable

--- addons/mil_ied/CfgFunctions.hpp
+++ addons/mil_ied/CfgFunctions.hpp
@@ -4,77 +4,77 @@ class cfgFunctions {
                         class IED {
                                 description = "The main class";
                                 file = "\x\alive\addons\mil_ied\fnc_ied.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class IEDInit {
                                 description = "The module initialisation function";
                                 file = "\x\alive\addons\mil_ied\fnc_iedInit.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class IEDMenuDef {
                                 description = "The module menu definition";
                                 file = "\x\alive\addons\mil_ied\fnc_iedMenuDef.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class AmbientBomber {
                                 description = "Create an ambient suicide bomber";
                                 file = "\x\alive\addons\mil_ied\fnc_createBomber.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class RemoveBomber {
                                 description = "Remove a suicide bomber";
                                 file = "\x\alive\addons\mil_ied\fnc_removeBomber.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class AmbientIED {
                                 description = "Create an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_createIED.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class RemoveIED {
                                 description = "Remove an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_removeIED.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class placeIED {
                                 description = "Find a suitable location for an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_placeIED.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class armIED {
                                 description = "Arm an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_armIED.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class createVBIED {
                                 description = "Create a VB-IED";
                                 file = "\x\alive\addons\mil_ied\fnc_createVBIED.sqf";
-								recompile = 1;
+								recompile = RECOMPILE;
                         };
                         class disarmIED {
                                 description = "Disarm a IED";
                                 file = "\x\alive\addons\mil_ied\fnc_disarmIED.sqf";
-                                                                recompile = 1;
+                                recompile = RECOMPILE;
                         };
                         class detectIED {
                                 description = "Detect a IED";
                                 file = "\x\alive\addons\mil_ied\fnc_detectIED.sqf";
-                                                                recompile = 1;
+                                recompile = RECOMPILE;
                         };
                         class createIED {
                                 description = "Create an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_createIED.sqf";
-                                                                recompile = 1;
+                                recompile = RECOMPILE;
                         };
                         class addActionIED {
                                 description = "Add an action to an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_addActionIED.sqf";
-                                                                recompile = 1;
+                                recompile = RECOMPILE;
                         };
                         class removeActionIED {
                                 description = "Remove an action from an IED";
                                 file = "\x\alive\addons\mil_ied\fnc_removeActionIED.sqf";
-                                                                recompile = 1;
+                                recompile = RECOMPILE;
                         };
                 };
         };
