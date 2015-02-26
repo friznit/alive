742f4c1bfee338ef7753940c092dbc8644036027
ARJay <arjaydev@gmail.com>
2015-02-26 22:25:40 +1100

    [mil_ied] changed sound

--- addons/mil_ied/CfgSounds.hpp
+++ addons/mil_ied/CfgSounds.hpp
@@ -2,10 +2,16 @@ class CfgSounds
 {
 	sounds[] = {};
 	
-		class Alive_Allah
+	class Alive_Allah
 	{
 		name = "Alive_Allah";
 		sound[] = {"x\alive\addons\mil_ied\audio\alive_akbar.ogg", 4, 1, 30};   
+		titles[] = {};
+	};
+	class Alive_scream
+	{
+		name = "Alive_Scream";
+		sound[] = {"x\alive\addons\mil_ied\audio\scream.ogg", 4, 1, 30};
 		titles[] = {};
 	};
 
