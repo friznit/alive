398408d4522315600523029f21ab0d5eec596cd7
Tupolov <tupolov73@gmail.com>
2015-09-14 01:08:02 +0100

    [ALiVE_Tablet] Fixed: Alive_tablet not detected by Virtual Arsenal #118 https://github.com/ALiVEmod/alive/issues/118

--- addons/c_tablet/CfgWeapons.hpp
+++ addons/c_tablet/CfgWeapons.hpp
@@ -3,11 +3,12 @@ class CfgWeapons {
 	class InventoryItem_Base_F;
 	class TabletItem: InventoryItem_Base_F
 	{
-		type = 1001;
+		type = 620;
 	};
 	class ALIVE_Tablet: ItemCore
 	{
 		scope = 2;
+		scopeArsenal=2;
 		displayName = "$STR_ALIVE_Tablet";
 		picture = "\x\alive\addons\m_tablet\data\UI\gear_tablet_CA.paa";
 		model = "\x\alive\addons\m_tablet\Tablet";
