61bbe706faab4b8797b210668f6cd378149ef485
highhead <1enterprise2>
2015-09-22 20:14:16 +0200

    [VARIOUS] Removed Combat support 2, module template and sys revive as not supported and abandoned

--- addons/sup_combatsupport2/config.cpp
+++
@@ -1,6 +0,0 @@
-#include <script_component.hpp>
-#include <CfgPatches.hpp>
-#include <CfgVehicles.hpp>
-#include <CfgFunctions.hpp>
-#include <eventhandlers.hpp>
-#include <\x\alive\addons\sup_combatsupport2\scripts\NEO_Radio\hpp\main.hpp>
